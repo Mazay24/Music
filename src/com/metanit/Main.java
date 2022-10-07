bcded68b5b4364f34f2ad6c9769e33c3de362ed5
Nikita <nik.isaev2004@mail.ru>
2022-10-07 23:43:56 +0300

    Add

--- src/com/metanit/Main.java
+++ src/com/metanit/Main.java
@@ -1,8 +1,13 @@
 package com.metanit;
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -13,42 +18,13 @@ public class Main {
     private static final String OUT_FILE_TXT = "outFile.txt";
     private static final String PATH_TO_MUSIC = "music\\music";
 
+    private static final String PILE_FOR_PICTURE = "inPicture.txt";
+    private static final String OUT_FILE_PICTURE = "outPicture.txt";
+    private static final String PATH_TO_PCTURE = "D:/Java/Musik//picture/image";
+
     public static void main(String[] args) {
-        String Url;
-        try (BufferedReader inFile = new BufferedReader(new FileReader(IN_FILE_TXT));
-             BufferedWriter outFile = new BufferedWriter(new FileWriter(OUT_FILE_TXT))) {
-            while ((Url = inFile.readLine()) != null) {
-                URL url = new URL(Url);
-
-                String result;
-                try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()))) {
-                    result = bufferedReader.lines().collect(Collectors.joining("\n"));
-                }
-                Pattern email_pattern = Pattern.compile("https:\\/\\/ru.hitmotop.com\\/get\\/music(.+).mp3");
-                Matcher matcher = email_pattern.matcher(result);
-                while (matcher.find()) {
-                    outFile.write(matcher.group() + "\r\n");
-
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try (BufferedReader musicFile = new BufferedReader(new FileReader(OUT_FILE_TXT))) {
-            String music;
-            int count = 1;
-            try {
-                while ((music = musicFile.readLine()) != null) {
-                    downloadUsingNIO(music, PATH_TO_MUSIC + String.valueOf(count) + ".mp3");
-                    count++;
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        downloadPicture();
+        downloadMusic();
     }
 
     private static void downloadUsingNIO(String strUrl, String file) throws IOException {
@@ -59,4 +35,89 @@ public class Main {
         stream.close();
         byteChannel.close();
     }
+
+public static void downloadMusic() {
+    String Url;
+    try (BufferedReader inFile = new BufferedReader(new FileReader(IN_FILE_TXT));
+         BufferedWriter outFile = new BufferedWriter(new FileWriter(OUT_FILE_TXT))) {
+        while ((Url = inFile.readLine()) != null) {
+            URL url = new URL(Url);
+
+            String result;
+            try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()))) {
+                result = bufferedReader.lines().collect(Collectors.joining("\n"));
+            }
+            Pattern email_pattern = Pattern.compile("https:\\/\\/ru.hitmotop.com\\/get\\/music(.+).mp3");
+            Matcher matcher = email_pattern.matcher(result);
+            while (matcher.find()) {
+                outFile.write(matcher.group() + "\r\n");
+
+            }
+        }
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+    try (BufferedReader musicFile = new BufferedReader(new FileReader(OUT_FILE_TXT))) {
+        String music;
+        int count = 1;
+        try {
+            while ((music = musicFile.readLine()) != null) {
+                downloadUsingNIO(music, PATH_TO_MUSIC + String.valueOf(count) + ".mp3");
+                count++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+
 }
+    public static void downloadPicture() {
+        String Url;
+        try (BufferedReader inFile = new BufferedReader(new FileReader(IN_FILE_TXT));
+             BufferedWriter outFile = new BufferedWriter(new FileWriter(OUT_FILE_PICTURE))) {
+            while ((Url = inFile.readLine()) != null) {
+                URL url = new URL(Url);
+
+                String result;
+                try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()))) {
+                    result = bufferedReader.lines().collect(Collectors.joining("\n"));
+                }
+
+
+                Pattern email_pattern = Pattern.compile("\\/covers\\/(.+).jpg");
+                Matcher matcher = email_pattern.matcher(result);
+                while (matcher.find()) {
+                    outFile.write("https://ru.hitmotop.com/" + matcher.group() + "\r\n");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (BufferedReader pictureFile = new BufferedReader(new FileReader(OUT_FILE_PICTURE))) {
+            String picture;
+            int count = 1;
+            try {
+                while ((picture = pictureFile.readLine()) != null) {
+                    try {
+                        InputStream in = new URL(picture).openStream();
+                        Files.copy(in, Paths.get(PATH_TO_PCTURE + count + ".jpg"));
+                        count++;
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }}
